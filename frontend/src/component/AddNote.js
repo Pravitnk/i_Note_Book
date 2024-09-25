5b08ca79d8aaaaad0b557a4529943d341d60cda3
PRAVIT PRASHANT NAIK <108272383+Pravitnk@users.noreply.github.com>
2024-09-25 12:49:11 +0530

    Update AddNote.js

--- frontend/src/component/AddNote.js
+++ frontend/src/component/AddNote.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext,useEffect } from "react";
 import noteContext from "../context/notes/notesContext";
 
 const AddNote = (props) => {
